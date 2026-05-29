7642479299801bf910d7089644de3ccc33bac767
Braylon <libr2023@mountunion.edu>
2026-05-29 17:14:25 -0400

    Added full Dungeon Slime Project + fixed .gitignore

---
+++ Dungeon Slime/Game1.cs
@@ -0,0 +1,78 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+
+namespace DungeonSlime;
+
+public class Game1 : Core
+
+{
+    // The MonoGame logo texture
+    Vector2 center = new Vector2(640f, 360f);
+    private Texture2D _logo;
+
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
+
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        // Clear the back buffer.
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin();
+
+        // Draw the texture.
+        SpriteBatch.Draw(
+            _logo,              // texture
+            new Vector2(        // position
+                (Window.ClientBounds.Width) - (_logo.Width * 0.5f),
+                (Window.ClientBounds.Height) - (_logo.Height * 0.5f)),
+            null,               // sourceRectangle
+            Color.White,        // color
+            0.0f,               // rotation
+            center,       // origin
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            0.0f                // layerDepth
+        );
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+
+
+
+
+}
